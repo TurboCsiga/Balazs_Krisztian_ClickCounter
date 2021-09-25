123b02b3e68e1325856444733d8e83f1b1c5a955
Jenő <bkrisz.100@gmail.com>
2021-09-25 17:46:39 +0200

    Színek kész

--- app/src/main/java/com/example/clickcounter/MainActivity.kt
+++ app/src/main/java/com/example/clickcounter/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.clickcounter
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textView : TextView
+    lateinit var buttonUp : Button
+    lateinit var buttonDown : Button
+    var num = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Init()
+
+        buttonUp.setOnClickListener{
+            num++
+            textView.setText(num.toString())
+            if (num == 0) {
+                textView.setTextColor(Color.rgb(0, 0, 255))
+            }
+            else if (num > 0) {
+                textView.setTextColor(Color.rgb(0, 255, 0))
+            }
+        }
+
+        buttonDown.setOnClickListener{
+            num--
+            textView.setText(num.toString())
+            if (num == 0) {
+                textView.setTextColor(Color.rgb(0, 0, 255))
+            }
+            else if (num < 0) {
+                textView.setTextColor(Color.rgb(255, 0, 0))
+            }
+        }
     }
+
+    fun Init()
+    {
+        textView = findViewById(R.id.tv_num)
+        textView.setText(num.toString())
+        buttonUp = findViewById(R.id.btn_top)
+        buttonDown = findViewById(R.id.btn_bottom)
+    }
+
 }
